fe90559f15fcd0c48b2b1e8754d01e29b69a480c
Regina Azzi <reginaazzi@hotmail.com>
2023-02-03 20:01:51 -0300

    Update If.java

--- estruturadecontroles/src/controle/If.java
+++ estruturadecontroles/src/controle/If.java
@@ -1,10 +1,33 @@
 package controle;
 
+import java.util.Scanner;
+
 public class If {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.print("Informe a média: ");
+        double media = entrada.nextDouble();
+        
+        if (media <= 10 && media>= 7.0);{
+        System.out.println("Aprovado");        
+        System.out.println("Parabens");
+        }
+        
+        if (media <7 && media >=4.50)
+        	System.out.println("Recuperação");
+        
+        if (media < 4.5 && media >= 0 )
+        	System.out.println("Reprovado");
+        
+        
+        
+        
+        entrada.close();
+        
+        
 	}
 
 }
